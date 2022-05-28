2bbbc8dd6f7e5f0481e71fcd56e64bdb4b0aa288
michalrys <michalrys@gmail.com>
2022-05-28 13:42:29 +0200

    LAB: simple example - email.

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,21 @@
 <template>
   <div>
+    <h1>Twój email to: {{ email }}.</h1>
+    <h2>Twój email to: {{ email }}.</h2>
+    <h3>Ilość znaków: {{email.length}}</h3>
+    <br>
+    <input type = "text" v-model = "email" >
 
   </div>
 </template>
 
 <script>
 export default {
+  data() {
+    return {
+      email: '',
+    }
+  }
 
 }
 </script>
